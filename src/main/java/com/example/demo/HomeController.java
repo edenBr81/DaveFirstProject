f8b213320ed1adfcef9b37508f50f011ff079635
Eden Bereda <edukenat@gmail.com>
2018-03-27 10:09:21 -0400

    description of changes

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-       // add a commment
+       // add a comment
     @RequestMapping("/loadForm")
     public String loadFormPage(){
         // System.out.println("in the loadForm method");
---
+++ src/main/java/com/example/demo/NewFeature.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class NewFeature {
+    private String description;
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+}
